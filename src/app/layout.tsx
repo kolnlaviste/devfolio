a8a635ea7365f600b642523fc3f136280e8bd975
Koln <koln.laviste@tekkio.com>
2026-03-24 13:58:40 +0800

    added: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,17 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "DevFolio",
   description: "Portfolio and works of Koln Roward Laviste",
+  icons: {
+    icon: [
+      { url: "/favicon/favicon.ico" }, 
+      { url: "/favicon/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+      { url: "/favicon/favicon.svg", type: "image/svg+xml" },
+    ],
+    apple: [
+      { url: "/favicon/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+  },
+  manifest: "/favicon/site.webmanifest",
 };
 
 export default function RootLayout({
